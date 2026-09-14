71b264d654b81d19e06ef1760be294e5f48ba250
wnck777 <antek.wi1306@gmail.com>
2026-09-14 22:16:07 +0200

    Added raw mode to the text editor and disabled terminal flags

--- text-editor.c
+++ text-editor.c
@@ -1,8 +1,44 @@
 #include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <termios.h>
+
+struct termios orig_termios;
+
+void disableRawMode(void)
+{
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+
+void enableRawMode(void)
+{
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+
+	struct termios raw = orig_termios;
+	raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= ~(CS8);
+	raw.c_lflag &= ~(ECHO | IEXTEN | ICANON | ISIG);
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+
 
 int main(void)
 {
+	enableRawMode();
+
 	char c;
-	while(read(STDIN_FILENO, &c, 1) == 1);
+	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+	{
+		if(iscntrl(c))
+			printf("%d\r\n",c);
+		else
+			printf("%d\t%c\r\n",c ,c);
+	}
 	return 0;
 }
