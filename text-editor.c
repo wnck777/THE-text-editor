0004a4a8590c91a680d4c203a5694b0b3cfbe6bc
wnck777 <antek.wi1306@gmail.com>
2026-09-15 21:23:41 +0200

    Added a welcome text

--- text-editor.c
+++ text-editor.c
@@ -12,6 +12,8 @@
 
 // defines
 
+#define TTE_VERSION "0.0.1"
+
 #define CTRL_KEY(key) ((key) & 0x1f)
 #define ABUF_INIT {NULL, 0}
 
@@ -125,13 +127,36 @@ void abFree(struct abuf *ab)
 
 // output
 
-void editorDrawTildas(struct abuf *ab)
+void editorDrawRows(struct abuf *ab)
 {
 	int y;
 	for(y = 0; y < E.screenrows; y++)
 	{
-		abAppend(ab, "~", 1);
+		if(y == E.screenrows / 3)
+		{
+			char welcome[80];
+			int welcomelen = snprintf(welcome,sizeof(welcome),
+				"The Text Editor - version %s", TTE_VERSION);
+			
+			if(welcomelen > E.screencols)
+				welcomelen = E.screencols;
 
+			int padding = (E.screencols - welcomelen) / 2;
+			
+			if(padding)
+			{
+				abAppend(ab, "~", 1);
+				padding--;
+			}
+
+			while(padding--)
+				abAppend(ab, " ", 1);
+
+			abAppend(ab, welcome, welcomelen);
+		} else
+			abAppend(ab, "~", 1);
+
+		abAppend(ab, "\x1b[K", 3);
 		if(y < E.screenrows - 1)
 			abAppend(ab, "\r\n", 2);
 	}
@@ -142,12 +167,11 @@ void editorRefresh(void)
 	struct abuf ab = ABUF_INIT;
 
 	abAppend(&ab, "\x1b[?25l", 6);
-	abAppend(&ab, "\x1b[2J", 4);
 	abAppend(&ab, "\x1b[H", 3);
 
-	editorDrawTildas(&ab);
+	editorDrawRows(&ab);
 
-	abAppend(&ab, "\x1b[H", 3);
+	abAppend(&ab, "\x1b[1;2H", 6);
 	abAppend(&ab, "\x1b[?25h", 6);
 
 	write(STDOUT_FILENO, ab.b, ab.len);
