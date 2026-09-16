097953e674363f8238c51d52a513a139deee1c22
wnck777 <antek.wi1306@gmail.com>
2026-09-16 19:09:26 +0200

    added detection of delete key

--- text-editor.c
+++ text-editor.c
@@ -26,7 +26,8 @@ enum editorkey
 	PAGE_UP,
 	PAGE_DOWN,
 	HOME_KEY,
-	END_KEY
+	END_KEY,
+	DEL_KEY
 };
 
 // data
@@ -100,6 +101,7 @@ int editorReadKey(void)
 					switch(seq[1])
 					{
 						case '1': return HOME_KEY;
+						case '3': return DEL_KEY;
 						case '4': return END_KEY;
 						case '5': return PAGE_UP;
 						case '6': return PAGE_DOWN;
